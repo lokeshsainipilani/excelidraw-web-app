6bfba82d137cff519da22f1cd4f2b6119e9a768f
Lokesh Saini <lokeshsainipilani@gmail.com>
2025-05-16 18:40:55 +0530

    s

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import {WebSocket, WebSocketServer } from "ws";
 import jwt, {JwtPayload} from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import {PrismaClient} from "@prisma/client"
+import {PrismaClient} from "@repo/db/client"
 import { createClient } from "redis";
 import { config } from "dotenv";
 config();
